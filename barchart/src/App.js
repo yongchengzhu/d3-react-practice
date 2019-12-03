60de2206bbd0bc67c8b7d76d269bce7749aa7873
yongchengzhu <yongchengzhu002@gmail.com>
2019-12-02 23:27:59 -0500

    Added dropdown.

--- barchart/src/App.js
+++ barchart/src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import ChartWrapper from './ChartWrapper';
+import GenderDropdown from './GenderDropdown';
 
 class App extends React.Component {
   render() {
@@ -14,6 +17,14 @@ class App extends React.Component {
           <Navbar.Brand>Barchart</Navbar.Brand>
         </Navbar>
         <Container>
+          <Row>
+            <Col xs={12}>
+              <GenderDropdown />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}></Col>
+          </Row>
           <ChartWrapper />
         </Container>
       </div>
---
+++ barchart/src/GenderDropdown.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+export default function GenderDropdown() {
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="primary" id="dropdown-basic">
+        Please Select Gender
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#">Men</Dropdown.Item>
+        <Dropdown.Item href="#">Women</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
